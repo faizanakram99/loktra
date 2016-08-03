c6c325dcbbf4002a8160dc1840ac1c72be41710e
faizanakram99 <faizanakram99@gmail.com>
2016-08-03 18:38:03 +0530

    completed!

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+angular.module('loktra', [])
+    .controller('mainCtrl', function($scope, $window){
+                $scope.total = 0;
+                $scope.left = 1000;
+                $scope.numbersOnly = /^\d+$/;
+                $scope.giveNow = function(){
+                    $scope.total += Number($scope.donate);
+                    if($scope.total > 1000){
+                        $scope.total -= Number($scope.donate);
+                        swal({
+                          title: "Warning!",
+                          text: "The total donation cannot exceed 1000!",
+                          type: "warning",
+                          confirmButtonText: "Ok"
+                        });
+                        $scope.donate = 0;
+                    }
+                    else
+                    {
+                        $scope.totalPercent = Number(($scope.total / 1000) * 100) + "%";
+                        $scope.left = Number(1000 - $scope.total);
+                        $scope.donate = 0;
+                    }
+                };
+                $scope.save = function(){
+                    swal({
+                          title: "Saved!",
+                          text: "Thank you!",
+                          type: "success",
+                          confirmButtonText: "Ok"
+                    });
+                }
+                $scope.share = function(){
+                   FB.login(function(){
+                        // Note: The call will only work if you accept the permission request
+                        FB.api('/me/feed', 'post', {message: 'Yay, I donated!'});
+                    }, {scope: 'publish_actions'});
+                };
+            });
